143a82a8bf06ea4ec6ce236d7e92defb945fae38
Ryan Bloom <ryan.bloom@levvel.io>
2019-08-27 16:17:25 -0400

    transform token to jwt response from oauth2 response

--- src/main/java/com/howtodoinjava/demo/config/OAuth2AuthorizationServer.java
+++ src/main/java/com/howtodoinjava/demo/config/OAuth2AuthorizationServer.java
@@ -1,12 +1,19 @@
 package com.howtodoinjava.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -36,5 +43,34 @@ public class OAuth2AuthorizationServer extends AuthorizationServerConfigurerAdap
 			.redirectUris("http://localhost:8081/login")
 			.accessTokenValiditySeconds(5000)
 			.refreshTokenValiditySeconds(50000);
+	}
+
+	@Bean
+	@Primary
+	public DefaultTokenServices tokenServices() {
+		DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+		defaultTokenServices.setTokenStore(tokenStore());
+		defaultTokenServices.setSupportRefreshToken(true);
+		return defaultTokenServices;
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints
+				.tokenStore(tokenStore())
+				.accessTokenConverter(accessTokenConverter());
+		//.authenticationManager(authenticationManager);
+	}
+
+	@Bean
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		final JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+		converter.setSigningKey("123");
+		return converter;
 	}
 }
